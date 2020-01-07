884fa865147fa59c849c77bf3ce2a81a7ab386c8
MichalMitros <mitrosmicha4@gmail.com>
2020-01-07 15:32:56 +0100

    File reading (positive records consolidator)

--- solution/data_preprocessors/positive_records_consolidator.py
+++ solution/data_preprocessors/positive_records_consolidator.py
@@ -10,9 +10,16 @@ import os
 
 file_count = 0
 
+def read_file(f):
+    if f.mode == 'r':
+        content = f.read()
+        print(content)
+    
+
 for dirpath, dnames, fnames in os.walk("../../data/"):
     for f in fnames:
         abspath = os.path.join(dirpath, f)
         with open(abspath) as f:
             file_count += 1
-            print("File noumber {}".format(file_count))
+            print("File number {}:".format(file_count))
+            read_file(f)
