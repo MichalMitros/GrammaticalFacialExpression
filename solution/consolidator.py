f981b50904c06553b339f9452fa67210c854cd69
Marek Parr <marek.parr@gmail.com>
2020-01-09 21:48:04 +0100

    added consolidator

--- solution/consolidator.py
+++ solution/consolidator.py
@@ -4,23 +4,20 @@ import os
     Reads positive records from data folder and
     consolidates them into one file with
     decission attribute added
-
-    TODO: finish...
+    
 '''
 
-file_count = 0
-
-
-def read_file(f):
-    if f.mode == 'r':
-        content = f.read()
-        print(content)
-
-
 for dirpath, dnames, fnames in os.walk("../data/"):
-    for f in fnames:
-        abspath = os.path.join(dirpath, f)
-        with open(abspath) as f:
-            file_count += 1
-            print("File number {}:".format(file_count))
-            read_file(f)
+    with open("../preprocessed_data/datafile.txt", "w") as datafile:
+        for i in range(0, len(fnames), 2):
+            datapoints_abspath = os.path.join(dirpath, fnames[i])
+            targets_abspath = os.path.join(dirpath, fnames[i + 1])
+            with open(datapoints_abspath) as datapoints:
+                with open(targets_abspath) as targets:
+                    targets_lines = targets.readlines()
+                    for j, line in enumerate(datapoints):
+                        if j != 0:
+                            if targets_lines[j-1] == "1\n":
+                                datafile.write(" ".join(line.split()[1:]) + " " + str(i//2) + "\n")
+                            else:
+                                datafile.write(" ".join(line.split()[1:]) + " -1\n")
