f9dc5a8ee934fbadcc15bafd2c1abaf7f6b25a7f
MichalMitros <mitrosmicha4@gmail.com>
2020-01-10 21:56:50 +0100

    implemented neural network with one hidden layer

--- solution/neural_network.py
+++ solution/neural_network.py
@@ -1,9 +1,84 @@
+import numpy as np
+
+
+def sigmoid(x, deriv=False):
+    if deriv:
+        return x * (1 - x)
+    return 1 / (1 + np.exp(-x))
+
+
 class NeuralNetwork:
 
-    def __init__(self, num_of_inputs, num_of_hidden, num_of_outputs):
-        self.num_of_inputs = num_of_inputs
-        self.num_of_hidden = num_of_hidden
-        self.num_of_outputs = num_of_outputs
+    def __init__(self, inputs, hidden, outputs, learning_rate=0.05, activation=sigmoid):
+        # temporary for debug porpoises
+        np.random.seed(1)
+        # set network parameters
+        self.inputs = inputs
+        self.hidden = hidden
+        self.outputs = outputs
+        self.learning_rate = learning_rate
+        self.activation = activation
+        # initialize weights ( matrices with values between -1 and 1)
+        self.hidden_weights = 2 * np.random.random((hidden, inputs)) - 1
+        self.output_weights = 2 * np.random.random((outputs, hidden)) - 1
+        # initialize biases (arrays with values between -1 and 1)
+        self.hidden_biases = 2 * np.random.rand(hidden) - 1
+        self.output_biases = 2 * np.random.rand(outputs) - 1
 
-    def print(self):
-        print("I am Neural Network!")
+    def feed_forward(self, input_array):
+        # Calculate hidden layer's output
+        hidden = np.dot(self.hidden_weights, input_array)
+        hidden = hidden + self.hidden_biases
+        hidden = self.activation(hidden)
+
+        # Calculate output's layer output (final result)
+        output = np.dot(self.output_weights, hidden)
+        output = output + self.output_biases
+        output = self.activation(output)
+
+        # return final result (array)
+        return output
+
+    # Choo choo!
+    def train(self, input_array, target_array):
+        # FEED FORWARD ALGORITHM:
+        # Calculate hidden layer's output
+        hidden = np.dot(self.hidden_weights, input_array)
+        hidden = hidden + self.hidden_biases
+        hidden = self.activation(hidden)
+
+        # Calculate output's layer output (final result)
+        output = np.dot(self.output_weights, hidden)
+        output = output + self.output_biases
+        output = self.activation(output)
+
+        # ERRORS CALCULATING
+        output_errors = target_array - output
+        hidden_errors = self.output_weights.T.dot(output_errors)
+
+        # BACKPROPAGATION
+        # Calculate the output layer's gradients
+        output_gradients = self.activation(output, deriv=True)
+        output_gradients = np.multiply(output_gradients, output_errors)
+        output_gradients = np.multiply(output_gradients, self.learning_rate)
+
+        # Calculate the output layer's deltas
+        output_deltas = np.dot(np.array([output_gradients]).T, np.array([hidden]))
+
+        # Adjusting output layer's weights and biases
+        self.output_weights = self.output_weights + output_deltas
+        self.output_biases = self.output_biases + output_gradients
+
+        # Calculate the hidden layer's gradients
+        hidden_gradients = self.activation(hidden, deriv=True)
+        hidden_gradients = np.multiply(hidden_gradients, hidden_errors)
+        hidden_gradients = np.multiply(hidden_gradients, self.learning_rate)
+
+        # Calculate the hidden layer's deltas
+        hidden_deltas = np.dot(np.array([hidden_gradients]).T, np.array([input_array]))
+
+        # Adjusting hidden layer's weights and biases
+        self.hidden_weights = self.hidden_weights + hidden_deltas
+        self.hidden_biases = self.hidden_biases + hidden_gradients
+
+        return (1./2.*len(target_array))*np.sum(np.square(output_errors))
--- solution/script.py
+++ solution/script.py
@@ -1,6 +1,42 @@
-from neural_network import NeuralNetwork as nn
+# NEURAL NETWORK TEST (XOR example)
+from neural_network import NeuralNetwork
+from random import shuffle
 
-nn = NeuralNetwork(2, 2, 1)
+# Training data
+# [x1, x2, y]
+# x1, x2 - attributes
+# y - decision
+data = [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+]
 
-nn.print()
+# Creating neural network with custom learning rate
+nn = NeuralNetwork(2, 3, 1, learning_rate=0.15)
 
+# Training
+for i in range(6000):
+    shuffle(data)
+    nn.train(data[0][:2], data[0][2:])
+    nn.train(data[1][:2], data[1][2:])
+    nn.train(data[2][:2], data[2][2:])
+    nn.train(data[3][:2], data[3][2:])
+
+# Restore original data (previous were shuffled)
+data = [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+]
+
+# Get results
+for i in range(0, len(data)):
+    result = nn.feed_forward(data[i][:2])
+    print('Got: {}\tExpected: {}\tError: {}'.format(
+        result,
+        data[i][2:],
+        abs(data[i][2:]-result))
+    )
