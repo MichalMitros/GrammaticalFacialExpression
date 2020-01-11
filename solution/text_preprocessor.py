e155d202e876805c8afa58f8b267d368f7e1a200
Marek Parr <marek.parr@gmail.com>
2020-01-11 13:25:39 +0100

    changed consolidator to text_preprocessor and added deleting unlabeled records

--- solution/text_preprocessor.py
+++ solution/text_preprocessor.py
@@ -3,12 +3,12 @@ import os
 '''
     Reads positive records from data folder and
     consolidates them into one file with
-    decission attribute added
-    
+    decision attribute added and unlabeled records deleted
+
 '''
 
 for dirpath, dnames, fnames in os.walk("../data/"):
-    with open("../preprocessed_data/datafile.txt", "w") as datafile:
+    with open("../preprocessed_data/datafile_v1.txt", "w") as datafile:
         for i in range(0, len(fnames), 2):
             datapoints_abspath = os.path.join(dirpath, fnames[i])
             targets_abspath = os.path.join(dirpath, fnames[i + 1])
@@ -17,7 +17,14 @@ for dirpath, dnames, fnames in os.walk("../data/"):
                     targets_lines = targets.readlines()
                     for j, line in enumerate(datapoints):
                         if j != 0:
-                            if targets_lines[j-1] == "1\n":
-                                datafile.write(" ".join(line.split()[1:]) + " " + str(i//2) + "\n")
+                            if targets_lines[j - 1] == "1\n":
+                                datafile.write(" ".join(line.split()[1:]) + " " + str(i // 2) + "\n")
                             else:
                                 datafile.write(" ".join(line.split()[1:]) + " -1\n")
+
+
+with open("../preprocessed_data/datafile_v1.txt") as datafile_v1:
+    with open("../preprocessed_data/datafile_v2.txt", "w") as datafile_v2:
+        for line in datafile_v1:
+            if line.split()[-1] != "-1":
+                datafile_v2.write(line)
