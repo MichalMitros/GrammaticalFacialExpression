36a5a5afdf4eed2267108fdd5237390689468f51
MichalMitros <mitrosmicha4@gmail.com>
2020-01-15 11:12:24 +0100

    add raport in tex, add accuracy to measure learning

---
+++ solution/main.py
@@ -0,0 +1,53 @@
+from math import floor
+from solution.file_preprocessor import consolidate_files, delete_unlabeled_records
+from solution.data_preprocessor import load_data, split_data
+from solution.neural_network import NeuralNetwork
+
+
+# Get class index from result
+def get_max_index(array):
+    index = 0
+    for i in range(len(array)):
+        if array[i] > array[index]:
+            index = i
+    return index
+
+
+# Let's do it every single time - why not?
+print('Preprocessing data...')
+consolidate_files()
+delete_unlabeled_records()
+
+# Load data and create neural network
+print('Loading data...')
+data = load_data('../preprocessed_data/datafile_v2.txt')
+data = split_data(data, 0.8)
+nn = NeuralNetwork(len(data[0][0]), 3*len(data[0][0]), 18)
+target_array = [0] * 18
+accuracy = 0
+epoch = 0
+
+print('Learning started...')
+while accuracy <= 0.9:
+    accuracy = 0.
+    epoch = epoch + 1
+
+    # Train:
+    for row_index, row in enumerate(data[0]):
+        target_array[int(data[1][row_index])] = 1.
+        nn.train(row, target_array)
+        target_array[int(data[1][row_index])] = 0.
+
+    # Test:
+    for row_index, row in enumerate(data[2]):
+        result = nn.feed_forward(row)
+        target_array[int(data[3][row_index])] = 1.
+        if get_max_index(result) == get_max_index(target_array):
+            accuracy = accuracy + 1.
+        target_array[int(data[3][row_index])] = 0.
+    accuracy = accuracy / len(data[3])
+
+    print('Epoch: {}\tAccuracy: {}%'.format(epoch, floor(accuracy*100)))
+
+print('Learning finished')
+
